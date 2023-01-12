af989caedff16a0cfb95eb332eb030e15b71dfae
Samy CHOUAM <sachouam@e2r1p12.clusters.42paris.fr>
2023-01-12 11:16:02 +0100

    files to set conda

---
+++ setconda.sh
@@ -0,0 +1,7 @@
+export CONDAPATH="/goinfre/$USER/miniconda3"
+curl -LO "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+sh Miniconda3-latest-Linux-x86_64.sh -b -p $CONDAPATH
+$CONDAPATH/bin/conda init zsh
+$CONDAPATH/bin/conda config --set auto_activate_base false
+source ~/.zshrc
+conda create --name 42AI-$USER python=3.7 jupyter pandas pycodestyle numpy
