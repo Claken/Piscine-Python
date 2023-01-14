11733ca2cc4a771603802ef6d712e44784f48b03
Samy CHOUAM <sachouam@e1r13p1.clusters.42paris.fr>
2023-01-14 11:50:06 +0100

    test before exercices

---
+++ module01/test.py
@@ -0,0 +1,20 @@
+def print_sum(argument="c'est bon ?", argument2="Pour toi"):
+	"""
+		C'est la doc bitch !
+		Cte fonction permet de faire des bailles interessants t'as capté
+	"""
+	print(argument, argument2)
+
+if __name__ == '__main__' :
+		s = """CA SENT LE "PD" CONNARD
+OK TA 'MERE'
+!!!
+		"""
+		s1 = str("je fais ce que je 'veux'")
+		print(s)
+		print(s1)
+		
+		print(s1[0:2])
+		print(s1[3:7])
+		print_sum(argument2="pd !?", argument="Tu te rappelles de moi")
+		# help(print_sum);
