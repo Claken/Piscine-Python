4435b756fb77cef3c46abde1efe8a8d5e8f65886
Claken <sachouam@student.42.fr>
2023-02-05 18:03:08 +0100

    01 en cours

--- module01/exec.py
+++ module01/exec.py
@@ -1,5 +1,10 @@
 import sys
 
 if __name__ == '__main__' :
-	print(sys.argv)
-	print(sys.argv)
+
+	joined = ""
+	for elem in sys.argv:
+		joined = "".join([joined, elem.swapcase()])
+		joined = "".join([joined, " "])
+	
+	print(joined)
