7665af7db3e1c9b726f26bcabed6eef5d8d8a0ba
Claken <sachouam@student.42.fr>
2023-02-06 17:09:33 +0100

    01 finished

--- module01/exec.py
+++ module01/exec.py
@@ -2,9 +2,15 @@ import sys
 
 if __name__ == '__main__' :
 
-	joined = ""
-	for elem in sys.argv:
-		joined = "".join([joined, elem.swapcase()])
-		joined = "".join([joined, " "])
-	
-	print(joined)
+	if len(sys.argv) > 1:
+		joined = ""
+		sys.argv.reverse()
+
+		for elem in sys.argv:
+			if elem != "exec.py":
+				elem = elem.swapcase()
+				for letter in reversed(elem):
+					joined = "".join([joined, letter])
+				if elem.swapcase() != sys.argv[len(sys.argv) - 2]:
+					joined = "".join([joined, " "])
+		print(joined)
