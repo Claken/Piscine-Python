262a176c5e02f207e70afc789209565f2cf42fec
Claken <sachouam@student.42.fr>
2023-02-09 16:16:43 +0100

    04 finished

--- module00/ex04/operations.py
+++ module00/ex04/operations.py
@@ -9,25 +9,23 @@ if __name__ == '__main__' :
 		python operations.py 10 3
 		""")
 	elif len(sys.argv) != 3:
-		print("You need TWO integers")
+		print("Error: You need TWO integers")
 	else:
 		try:
 			integer01 = int(sys.argv[1])
-		except:
-			print('The first argument is not an integer')
-		try:
 			integer02 = int(sys.argv[2])
 		except:
-			print('The second argument is not an integer')
+			print('Error: only integers are valid arguments')
 		else:
 			add = integer01 + integer02
 			min = integer01 - integer02
 			tim = integer01 * integer02
-			div = integer01 / integer02
-			mod = integer01 % integer02
 			print("Sum       : " + str(add))
 			print("Difference: " + str(min))
 			print("Product   : " + str(tim))
-			print("Quotient  : " + str(div))
-			print("Remainer  : " + str(mod))
+			if integer02 != 0:
+				div = integer01 / integer02
+				mod = integer01 % integer02
+			print("Quotient  : " + (str(div) if integer02 != 0 else "ERROR (division by zero)"))
+			print("Remainer  : " + (str(mod) if integer02 != 0 else "ERROR (modulo by zero)"))
 
