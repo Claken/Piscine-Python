49370dfc736bd3009215a1a65ea70446bba7ca38
Claken <samychouam@gmail.com>
2023-02-11 14:04:07 +0100

    some exos normed

--- module00/ex01/exec.py
+++ module00/ex01/exec.py
@@ -1,16 +1,16 @@
 import sys
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
 
-	if len(sys.argv) > 1:
-		joined = ""
-		sys.argv.reverse()
+    if len(sys.argv) > 1:
+        joined = ""
+        sys.argv.reverse()
 
-		for elem in sys.argv:
-			if elem != "exec.py":
-				elem = elem.swapcase()
-				for letter in reversed(elem):
-					joined = "".join([joined, letter])
-				if elem.swapcase() != sys.argv[len(sys.argv) - 2]:
-					joined = "".join([joined, " "])
-		print(joined)
+        for elem in sys.argv:
+            if elem != "exec.py":
+                elem = elem.swapcase()
+                for letter in reversed(elem):
+                    joined = "".join([joined, letter])
+                if elem.swapcase() != sys.argv[len(sys.argv) - 2]:
+                    joined = "".join([joined, " "])
+        print(joined)
--- module00/ex04/operations.py
+++ module00/ex04/operations.py
@@ -1,31 +1,30 @@
 import sys
 import math
 
-if __name__ == '__main__' :
-	if len(sys.argv) <= 1:
-		print("""
-		Usage: python operations.py <number1> <number2>
-		Example:
-		python operations.py 10 3
-		""")
-	elif len(sys.argv) != 3:
-		print("Error: You need TWO integers")
-	else:
-		try:
-			integer01 = int(sys.argv[1])
-			integer02 = int(sys.argv[2])
-		except:
-			print('Error: only integers are valid arguments')
-		else:
-			add = integer01 + integer02
-			min = integer01 - integer02
-			tim = integer01 * integer02
-			print("Sum       : " + str(add))
-			print("Difference: " + str(min))
-			print("Product   : " + str(tim))
-			if integer02 != 0:
-				div = integer01 / integer02
-				mod = integer01 % integer02
-			print("Quotient  : " + (str(div) if integer02 != 0 else "ERROR (division by zero)"))
-			print("Remainer  : " + (str(mod) if integer02 != 0 else "ERROR (modulo by zero)"))
-
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("""
+        Usage: python operations.py <number1> <number2>
+        Example:
+        python operations.py 10 3
+        """)
+    elif len(sys.argv) != 3:
+        print("Error: You need TWO integers")
+    else:
+        try:
+            integer01 = int(sys.argv[1])
+            integer02 = int(sys.argv[2])
+        except ValueError:
+            print('Error: only integers are valid arguments')
+        else:
+            add = integer01 + integer02
+            min = integer01 - integer02
+            tim = integer01 * integer02
+            print("Sum       : " + str(add))
+            print("Difference: " + str(min))
+            print("Product   : " + str(tim))
+            if integer02 != 0:
+                div = integer01 / integer02
+                mod = integer01 % integer02
+            print("Quotient  : " + (str(div) if integer02 != 0 else "ERROR (division by zero)"))
+            print("Remainer  : " + (str(mod) if integer02 != 0 else "ERROR (modulo by zero)"))
