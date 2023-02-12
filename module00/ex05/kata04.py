3d12cf1006c55c1d6a5d67aa75d9ea2cfb9fccd5
Claken <sachouam@student.42.fr>
2023-02-12 02:27:16 +0100

    05 finished

--- module00/ex05/kata04.py
+++ module00/ex05/kata04.py
@@ -1,2 +1,6 @@
 # Put this at the top of your kata04.py file
 kata = (0, 4, 132.42222, 10000, 12345.67)
+
+if __name__ == '__main__':
+    new_string = "module_{:02d}, ex_{:02d} : {:.2f}, {:.2e}, {:.2e}"
+    print(new_string.format(kata[0], kata[1], kata[2], kata[3], kata[4]))
