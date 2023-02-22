a7ea7b0745260e98ab8aea85f0d1957b2f2889c0
Claken <sachouam@student.42.fr>
2023-02-22 18:19:54 +0100

    ex06 en cours

--- module00/ex06/recipe.py
+++ module00/ex06/recipe.py
@@ -1,13 +1,10 @@
-cookbook = {
-    'ingredients': {},
-    'meal': "",
-    'prep_time': 0,
+cookbook = {'Sandwich': {'ingredients': ['ham', 'bread', 'cheese', 'tomatoes'],'meal':'lunch', 'prep_time': 10}, 
+            'Cake': {'ingredients': ['flour', 'sugar', 'eggs'], 'meal': 'dessert', 'prep_time': 60},
+            'Salad': {'ingredients': ['avocado', 'arugula', 'tomatoes', 'spinach'], 'meal': 'lunch', 'prep_time': 15}
 }
 
+def delete_recipe(name):
+    del cookbook[name]
+
 if __name__ == "__main__":
-    testbook = cookbook
-    testbook['ingredients'] = {'ham', 'bread', 'cheese', 'tomatoes'}
-    testbook['meal'] = 'lunch'
-    testbook['prep_time'] = 10
-    print(cookbook)
-    print(testbook)
+    print(cookbook['Cake'])
