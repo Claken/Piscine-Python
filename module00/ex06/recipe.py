f8ed24b25a934633ed8847c3c0998e5e2751bb03
Claken <samychouam@gmail.com>
2023-02-23 12:18:47 +0100

    ex06 toujours en cours

--- module00/ex06/recipe.py
+++ module00/ex06/recipe.py
@@ -3,8 +3,36 @@ cookbook = {'Sandwich': {'ingredients': ['ham', 'bread', 'cheese', 'tomatoes'],'
             'Salad': {'ingredients': ['avocado', 'arugula', 'tomatoes', 'spinach'], 'meal': 'lunch', 'prep_time': 15}
 }
 
+def print_recipe_names():
+    receplist = list(cookbook)
+    for names in receplist:
+        print(names)
+
+def print_recipe_details(name):
+    print(cookbook[name])
+
 def delete_recipe(name):
     del cookbook[name]
 
+def add_a_recipe():
+    name = input("Enter a name: \n")
+    ingredients = []
+    ingredient = "test"
+    print("Enter ingredients (type ENTER without writing when you done): \n")
+    while ingredient != "":
+        test = input()
+        if test == "":
+            continue
+        ingredients.append(test)
+    meal = input("Enter a meal type: \n")
+    time = int(input("Enter a preparation time: \n"))
+    cookbook[name] = {'ingredients' : ingredients, 'meal' : meal, 'prep_time' : time}
+
 if __name__ == "__main__":
-    print(cookbook['Cake'])
+    """ print_recipe_details('Sandwich')
+    print_recipe_details('Cake')
+    print_recipe_details('Salad')
+    delete_recipe('Cake')
+    print_recipe_names() """
+    add_a_recipe()
+    print(cookbook)
