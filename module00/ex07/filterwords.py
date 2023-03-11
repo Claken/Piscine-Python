5bfbd6145b28289a4f15d720b0b88fc7c945be93
Claken <sachouam@student.42.fr>
2023-03-11 03:29:53 +0100

    ex07 fini

--- module00/ex07/filterwords.py
+++ module00/ex07/filterwords.py
@@ -1,6 +1,15 @@
 import sys
 import string
 
+def filterwords(words):
+	newwords = []
+	for word in words:
+		for char in word:
+			if char in string.punctuation:
+				word = word.replace(char, '')
+		newwords.append(word)
+	return newwords
+
 if __name__ == "__main__":
 	if len(sys.argv) != 3:
 		print("ERROR: Invalid number of arguments")
@@ -8,12 +17,12 @@ if __name__ == "__main__":
 		try:
 			n = int(sys.argv[2])
 		except ValueError:
-			print("ERROR: Invalid argument")
+			print("ERROR: argument must be an integer")
 		else:
 			if n > 0:
 				words = sys.argv[1].split()
+				words = filterwords(words)
 				words = [word for word in words if len(word) > n]
-
 				print(words)
 			else:
 				print("ERROR: size must be superior to zero")
