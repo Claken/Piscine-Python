3466b3eef260cd75101f952c50889c06c9c737ea
Claken <samychouam@gmail.com>
2023-03-20 00:46:06 +0100

    ex10 en cours toujours

--- module00/ex10/loading.py
+++ module00/ex10/loading.py
@@ -1,10 +1,16 @@
 from tqdm import tqdm
+from time import sleep
 
 def ft_progress(listeuh):
     for i in tqdm(listeuh, desc="ETA: ", ascii=' >=', ncols=100):
-        #yield i
+        yield i
         pass
 
 if __name__ == "__main__":
-    listeuh = range(int(9e6))
-    ft_progress(listeuh)
+    listeuh = range(1000)
+    ret = 0
+    for elem in ft_progress(listeuh):
+        ret += (elem + 3) % 5
+        sleep(0.001)
+    print()
+    print(ret)
