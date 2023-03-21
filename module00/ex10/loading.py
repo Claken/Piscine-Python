0064634295fd4657e2e14158effa5c14bfb68ade
Claken <samychouam@gmail.com>
2023-03-21 11:29:59 +0100

    mise à la norme de l'exo10

--- module00/ex10/loading.py
+++ module00/ex10/loading.py
@@ -1,16 +1,21 @@
 from tqdm import tqdm
 from time import sleep
 
+
 def ft_progress(listeuh):
-    for i in tqdm(listeuh, desc="ETA: ", ascii=' >=', ncols=90, bar_format="{desc}{remaining_s:.2f}s [{percentage:3.0f}%][{bar}] {n_fmt}/{total_fmt} | elapsed time {elapsed_s:.2f}s"):
+    for i in tqdm(listeuh, desc="ETA: ", ascii=' >=',
+                  ncols=90, bar_format="\
+{desc}{remaining_s:.2f}s [{percentage:3.0f}%][{bar}] \
+{n_fmt}/{total_fmt} | elapsed time {elapsed_s:.2f}s"):
         yield i
         pass
 
+
 if __name__ == "__main__":
-    listeuh = range(1000)
+    listy = range(3333)
     ret = 0
-    for elem in ft_progress(listeuh):
-        ret += (elem + 3) % 5
-        sleep(0.01)
+    for elem in ft_progress(listy):
+        ret += elem
+        sleep(0.005)
     print()
     print(ret)
