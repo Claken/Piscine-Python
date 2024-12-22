7dd42adc5b8e0689862887e0407476dae7aeb6b5
sachou <sachou@UBUNTU.myguest.virtualbox.org>
2024-12-22 19:09:22 +0100

    ex03 in progress

---
+++ python_0_Starting/ex03/NULL_not_found.py
@@ -0,0 +1,7 @@
+def NULL_not_found(object: any) -> int:
+	if object is None or object is "NaN" or object is 0 or object is "" or object is False:
+		print(f"{object}: {object} {type(object)}")
+		return 0
+	else:
+		print("Type not Found")
+	return 1
