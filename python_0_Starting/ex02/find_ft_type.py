dfb20cd6323223e7cabe2f482af70a7c421b88b6
sachou <sachou@UBUNTU.myguest.virtualbox.org>
2024-12-22 14:59:06 +0100

    ex02 done

--- python_0_Starting/ex02/find_ft_type.py
+++ python_0_Starting/ex02/find_ft_type.py
@@ -1,9 +1,11 @@
 def all_thing_is_obj(object: any) -> int:
 	str = ""
 	if type(object).__name__ == "str":
-		str = object + " is in the kitchen"
-	else:
+		str = f"{object} is in the kitchen : {type(object)}"
+	elif isinstance(object, list) | isinstance(object, tuple) | isinstance(object, set) | isinstance(object, dict):
 		str = type(object).__name__
-		str = str[0].upper() + str[1:]
-	print(str, ":", type(object))
+		str = f"{str[0].upper()}{str[1:]} : {type(object)}"
+	else:
+		str = "Type not found"
+	print(str)
 	return 42
