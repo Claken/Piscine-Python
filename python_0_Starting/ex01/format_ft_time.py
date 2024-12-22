fc7445904cc6b9637930b5a536ee66c444d56f21
sachou <sachou@UBUNTU.myguest.virtualbox.org>
2024-12-22 01:41:43 +0100

    ex01 done

--- python_0_Starting/ex01/format_ft_time.py
+++ python_0_Starting/ex01/format_ft_time.py
@@ -1,6 +1,9 @@
 import time
-from datetime import datetime
 
-if __name__ == "__main__":
-	maintenant = datetime.now()
-	print("Date et heure actuelles :", maintenant)
+current_time = time.time()
+formatted_time = f"{current_time:,.4f}"
+scientific_notation = f"{current_time:.2e}"
+print(f"Seconds since January 1, 1970: {formatted_time} or {scientific_notation} in scientific notation")
+local_time = time.localtime(current_time)
+formatted_date = time.strftime("%b %d %Y", local_time)
+print(formatted_date)
