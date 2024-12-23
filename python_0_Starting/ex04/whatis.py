c6ddc8a60a6412910625e77f07207bae0d2820c4
sachou <sachou@UBUNTU.myguest.virtualbox.org>
2024-12-23 17:24:09 +0100

    ex04 in progress

--- python_0_Starting/ex04/whatis.py
+++ python_0_Starting/ex04/whatis.py
@@ -1,4 +1,20 @@
 import sys
 
-print(sys.argv)
-print(sys.argv[1])
+if len(sys.argv) > 2:
+	print("AssertionError: more than one argument is provided")
+	sys.exit(1)
+
+if len(sys.argv) != 2:
+	# print("Usage: python whatis.py <number>")
+	sys.exit(1)
+
+number: int = int(sys.argv[1])
+
+# if ValueError:
+# 	print("AssertionError: argument is not an integer")
+# 	sys.exit(1)
+
+if number % 2 == 0:
+	print("I'm Even")
+elif number % 2 != 0:
+	print("I'm Odd")
