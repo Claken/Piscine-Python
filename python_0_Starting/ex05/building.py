18d53977437101d40310a3f2fa9c6ef1bfcd19ba
sachou <sachou@UBUNTU.myguest.virtualbox.org>
2024-12-25 17:30:42 +0100

    ex05 still in progress

--- python_0_Starting/ex05/building.py
+++ python_0_Starting/ex05/building.py
@@ -9,8 +9,6 @@ def text_analyzer(text=None):
     """
     if text is None:
         text = input("What is the text to count ?\n")
-    elif type(text) != str:
-        print("Error: this argument is not a string")
 
     characters = len(text)
     ups = sum(1 for c in text if c.isupper())
@@ -20,13 +18,12 @@ def text_analyzer(text=None):
     punct = sum(1 for c in text if c in string.punctuation)
     digits = sum(1 for c in text if c.isdigit())
 
-    print("The text contains " + str(characters) + " character(s):")
-    print("- " + str(ups) + " upper letter(s)")
-    print("- " + str(lows) + " lower letter(s)")
-    print("- " + str(punct) + " punctuation mark(s)")
-    print("- " + str(spaces) + " spaces(s)")
-    print("- " + str(digits) + " digit(s)")
-
+    print("The text contains " + str(characters) + f" character{'s' if characters > 1 else ''}:")
+    print(f"- {str(ups)} upper letter{'s' if ups > 1 else ''}")
+    print(f"- {str(lows)} lower letter{'s' if lows > 1 else ''}")
+    print(f"- {str(punct)} punctuation mark{'s' if punct > 1 else ''}")
+    print(f"- {str(spaces)} space{'s' if spaces > 1 else ''}")
+    print(f"- {str(digits)} digit{'s' if digits > 1 else ''}")
 
 if __name__ == '__main__':
 	if len(sys.argv) > 2:
