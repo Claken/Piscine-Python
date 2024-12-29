12cfb7c60ef7c5ce52d1d91b718acd01a4418cd9
sachou <sachou@UBUNTU.myguest.virtualbox.org>
2024-12-29 15:38:48 +0100

    ex09 in progress still

---
+++ python_0_Starting/ex09/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='ft_package',
+	version='0.0.1',
+	packages=find_packages(),
+	description='A sample test package',
+	author='sachouam',
+	author_email='sachouam@student.42.fr',
+	url=": https://github.com/Claken/ft_package",
+)
