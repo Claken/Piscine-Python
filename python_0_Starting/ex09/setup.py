2be8bee16a4006dabecc8c073f39602b12f90748
sachou <sachou@UBUNTU.myguest.virtualbox.org>
2024-12-29 19:18:06 +0100

    ex09 still in progress

--- python_0_Starting/ex09/setup.py
+++ python_0_Starting/ex09/setup.py
@@ -4,8 +4,15 @@ setup(
 	name='ft_package',
 	version='0.0.1',
 	packages=find_packages(),
-	description='A sample test package',
+	description=open("README.md").read(),
 	author='sachouam',
 	author_email='sachouam@student.42.fr',
 	url=": https://github.com/Claken/ft_package",
+	classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[], 
 )
