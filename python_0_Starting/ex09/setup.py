cb4c8a4b8646b8858cf8a172811fb222fd694490
sachou <sachou@UBUNTU.myguest.virtualbox.org>
2024-12-30 17:28:30 +0100

    ex09 with dist dir among others

--- python_0_Starting/ex09/setup.py
+++ python_0_Starting/ex09/setup.py
@@ -3,16 +3,11 @@ from setuptools import setup, find_packages
 setup(
 	name='ft_package',
 	version='0.0.1',
+	metadata_version='2.1',
 	packages=find_packages(),
 	description=open("README.md").read(),
+	license='MIT',
 	author='sachouam',
 	author_email='sachouam@student.42.fr',
 	url=": https://github.com/Claken/ft_package",
-	classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-    install_requires=[], 
 )
