f9fc9e197e6e2d8daf859cdcd933dac80f09b56d
Samy CHOUAM <sachouam@bess-f2r4s5.clusters.42paris.fr>
2025-01-03 19:53:39 +0100

    almost everything normed

--- python_0_Starting/ex05/building.py
+++ python_0_Starting/ex05/building.py
@@ -1,17 +1,19 @@
 import string
 import sys
 
+
 def text_analyzer(text=None):
+
     """
     This function counts the number of upper characters,
     lower characters, punctuation, spaces and digits in a given text.
-	if text is None, the user is prompted to enter a text.
-	
-	:param text: The text to analyze.
+    if text is None, the user is prompted to enter a text.
+
+    :param text: The text to analyze.
     """
     if text is None:
         print("What is the text to count?")
-        text = user_input = sys.stdin.readline()
+        text = sys.stdin.readline()
 
     characters = len(text)
     ups = sum(1 for c in text if c.isupper())
@@ -21,17 +23,20 @@ def text_analyzer(text=None):
     punct = sum(1 for c in text if c in string.punctuation)
     digits = sum(1 for c in text if c.isdigit())
 
-    print(f"The text contains {str(characters)} character{'s' if characters > 1 else ''}:")
+    plural = 's' if characters > 1 else ''
+    print(f"The text contains {str(characters)} character{plural}:")
     print(f"- {str(ups)} upper letter{'s' if ups > 1 else ''}")
     print(f"- {str(lows)} lower letter{'s' if lows > 1 else ''}")
     print(f"- {str(punct)} punctuation mark{'s' if punct > 1 else ''}")
     print(f"- {str(spaces)} space{'s' if spaces > 1 else ''}")
     print(f"- {str(digits)} digit{'s' if digits > 1 else ''}")
 
+
 if __name__ == '__main__':
-	if len(sys.argv) > 2:
-		print("AssertionError: wrong number of arguments")
-	elif len(sys.argv) == 2:
-		text_analyzer(sys.argv[1])    
-	else:
-		text_analyzer()
+
+    if len(sys.argv) > 2:
+        print("AssertionError: wrong number of arguments")
+    elif len(sys.argv) == 2:
+        text_analyzer(sys.argv[1])
+    else:
+        text_analyzer()
--- python_0_Starting/ex06/filterstring.py
+++ python_0_Starting/ex06/filterstring.py
@@ -1,11 +1,12 @@
 import sys
 from ft_filter import ft_filter
 
+
 def contains_special_characters(s: str) -> bool:
     """
-	Check if a string contains special characters, except for spaces
-	
-	:param s: The string to check.
+    Check if a string contains special characters, except for spaces
+
+    :param s: The string to check.
     """
     for char in s:
         if char.isalnum() is False and char != ' ':
@@ -13,28 +14,31 @@ def contains_special_characters(s: str) -> bool:
             return True
     return False
 
-def ft_filter_string(string: str, integer: int):
-	"""
-	Returns a list of words from "string" that have a length greater than "integer".
-	Words are separated from each other by space characters.
 
-	:param string: The string to filter.
-	:param integer: The minimum length of the words to keep.
-	"""
-	condition = lambda aString: len(aString) > integer
-	strTab = string.split()
-	return ft_filter(condition, strTab)
+def ft_filter_string(string: str, integer: int):
+    """
+    Returns a list of words from "string"
+    Each word had to have a length greater than "integer".
+    Words are separated from each other by space characters.
+
+    :param string: The string to filter.
+    :param integer: The minimum length of the words to keep.
+    """
+    def condition(aString): return len(aString) > integer
+    strTab = string.split()
+    return ft_filter(condition, strTab)
+
 
 if __name__ == '__main__':
-	if len(sys.argv) != 3:
-		print("AssertionError: wrong number of arguments")
-		sys.exit(1)
-	try:
-		strToFilter = str(sys.argv[1])
-		length = int(sys.argv[2])
-		if (contains_special_characters(strToFilter)):
-			sys.exit(1)
-		print(ft_filter_string(strToFilter, length))
-	except ValueError:
-		print("AssertionError: the arguments are bad")
-		sys.exit(1)
+    if len(sys.argv) != 3:
+        print("AssertionError: wrong number of arguments")
+        sys.exit(1)
+    try:
+        strToFilter = str(sys.argv[1])
+        length = int(sys.argv[2])
+        if (contains_special_characters(strToFilter)):
+            sys.exit(1)
+        print(ft_filter_string(strToFilter, length))
+    except ValueError:
+        print("AssertionError: the arguments are bad")
+        sys.exit(1)
