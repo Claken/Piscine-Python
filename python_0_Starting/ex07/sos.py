5e16066e691e1fb979807f4199bbb26104e1f51d
Samy CHOUAM <sachouam@bess-f2r4s5.clusters.42paris.fr>
2025-01-03 20:09:27 +0100

    no space at the end for sos

--- python_0_Starting/ex07/sos.py
+++ python_0_Starting/ex07/sos.py
@@ -68,9 +68,9 @@ if __name__ == "__main__":
         oneString = concatenateStrings(sys.argv[1:])
         oneString = oneString.upper()
         if checkString(oneString) is False:
-            print("Error: characters in string must be spaces and \
+            print("AssertionError: characters in string must be spaces and \
 alphanumeric (a-z, A-Z, 0-9)")
         else:
             for char in oneString:
-                print(CHARS_TO_MORSE[char], end=" ")
+                print(CHARS_TO_MORSE[char], end="")
             print("")
