23a66f2e587a94d4f738754bf8c07f9d3bfe8138
sachou <sachou@UBUNTU.myguest.virtualbox.org>
2025-02-04 19:30:36 +0100

    exercice 04 corrected

--- python_0_Starting/ex04/whatis.py
+++ python_0_Starting/ex04/whatis.py
@@ -1,19 +1,22 @@
 import sys
 
-if len(sys.argv) > 2:
-	print("AssertionError: more than one argument is provided")
-	sys.exit(1)
+try: 
+    length: int = len(sys.argv)
+    assert length < 3, "more than one argument is provided"
+except AssertionError as e:
+    print(f"{type(e).__name__}: {e}")
 
 if len(sys.argv) != 2:
 	sys.exit(1)
 
 try:
-    number: int = int(sys.argv[1])
-except ValueError:
-    print("AssertionError: argument is not an integer")
-    sys.exit(1)
+    assert sys.argv[1].isdigit(), "argument is not an integer"
+    number = int(sys.argv[1])
+    if number % 2 == 0:
+        print("I'm Even")
+    elif number % 2 != 0:
+        print("I'm Odd")
+except AssertionError as e:
+    print(f"{type(e).__name__}: {e}")
 
-if number % 2 == 0:
-	print("I'm Even")
-elif number % 2 != 0:
-	print("I'm Odd")
+
